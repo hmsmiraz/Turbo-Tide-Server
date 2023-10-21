df3f4921da711c601779c8043d12b9a00f63d614
hmsmiraz <hmsmiraz64729@gmail.com>
2023-10-21 15:36:22 +0600

    added user and cart db

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ async function run() {
 
     const brandCollection = client.db("brandDB").collection("brand");
     const productsCollection = client.db("brandDB").collection("products");
+    const userCollection = client.db("brandDB").collection("users");
+    const cartCollection = client.db("brandDB").collection("cart");
 
     // products data
     app.get("/products", async (req, res) => {
@@ -51,6 +53,28 @@ async function run() {
       res.send(result);
     });
 
+    app.put('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const updateProduct = req.body;
+
+      const product = {
+          $set: {
+            productName: updateProduct.productName,
+            brandName: updateProduct.brandName,
+            ProductType: updateProduct.ProductType,
+            Price: updateProduct.Price,
+            rating: updateProduct.rating,
+            picture: updateProduct.picture,
+          }
+      }
+
+      const result = await productsCollection.updateOne(filter, product, options);
+      res.send(result);
+      console.log(result)
+  })
+
     // Brands data
     app.get("/brand", async (req, res) => {
       const cursor = brandCollection.find();
@@ -65,6 +89,55 @@ async function run() {
       res.send(result);
     });
 
+    // user related apis
+    app.get('/users', async (req, res) => {
+      const cursor = userCollection.find();
+      const users = await cursor.toArray();
+      res.send(users);
+  })
+
+  app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+  });
+
+  app.patch('/users', async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email }
+      const updateDoc = {
+          $set: {
+            uid: user.uid,
+            lastLoggedAt: user?.lastLoggedAt,
+          }
+      }
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+      console.log(result)
+  })
+
+      // cart related apis
+      app.get('/cart', async (req, res) => {
+        const cursor = cartCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+  
+    app.post('/cart', async (req, res) => {
+        const newItem = req.body;
+        console.log(newItem);
+        const result = await cartCollection.insertOne(newItem);
+        res.send(result);
+        console.log(result);
+    });
+    app.delete('/cart/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+      console.log(result);
+  })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
